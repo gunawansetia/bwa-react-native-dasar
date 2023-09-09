444725a0581c4cdbaa88ff1a07264340da894655
gunawansetia <newgunawansetiawiguna@gmail.com>
2023-09-10 01:25:36 +0700

    Export dan Import Component
    * belajar react native dasar bwa

--- App.tsx
+++ App.tsx
@@ -1,130 +1,17 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, {Component} from 'react';
-import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
-import macbook from './macbook.jpg';
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+import StylingReactNativeComponent from './StylingReactNativeComponent';
+import SampleComponent from './SampleComponent';
 
 const App = () => {
-  return <StylingReactNativeComponent />;
-};
-
-const StylingReactNativeComponent = () => {
   return (
     <View>
-      <Text style={styles.text}>Styling Component</Text>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: '#0abde3',
-          borderWidth: 2,
-          borderColor: '#5f27cd',
-          marginTop: 20,
-          marginLeft: 20,
-        }}
-      />
-      <View
-        style={{
-          padding: 12,
-          backgroundColor: '#F2F2F2',
-          width: 212,
-          borderRadius: 8,
-        }}>
-        <Image
-          source={macbook}
-          style={{width: 188, height: 107, borderRadius: 8}}
-        />
-        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: '16'}}>
-          New Macbook Pro 2019
-        </Text>
-        <Text
-          style={{
-            fontSize: 12,
-            fontWeight: 'bold',
-            color: '#F2994A',
-            marginTop: 12,
-          }}>
-          Rp25.000.000
-        </Text>
-
-        <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-          Jakarta Barat
-        </Text>
-        <View
-          style={{
-            backgroundColor: '#6FCF97',
-            paddingVertical: 6,
-            borderRadius: 25,
-            marginTop: 20,
-          }}>
-          <Text
-            style={{
-              fontSize: 14,
-              fontWeight: '600',
-              color: 'white',
-              textAlign: 'center',
-            }}>
-            BELI
-          </Text>
-        </View>
-      </View>
+      <ScrollView>
+        <SampleComponent />
+        <StylingReactNativeComponent />
+      </ScrollView>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#10ac84',
-    marginLeft: 20,
-    marginTop: 40,
-  },
-});
-
-const SampleComponent = () => {
-  return (
-    <View>
-      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
-      <Text>Prawito</Text>
-      <Hudoro />
-      <Text>Mila</Text>
-      <Text>Azzamy</Text>
-      <Photo />
-      <TextInput style={{borderWidth: 1}} />
-      <Profile />
-    </View>
-  );
-};
-
-const Hudoro = () => {
-  return <Text>Hudoro Mangku Negoro</Text>;
-};
-
-const Photo = () => {
-  return (
-    <Image
-      source={{
-        uri: 'https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-      }}
-      style={{width: 100, height: 100}}
-    />
-  );
-};
-
-class Profile extends Component {
-  render() {
-    return (
-      <View>
-        <Image
-          source={{
-            uri: 'https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-          }}
-          style={{width: 100, height: 100, borderRadius: 50}}
-        />
-        <Text style={{color: 'blue', fontSize: 24}}>Ini Gambar</Text>
-      </View>
-    );
-  }
-}
 
 export default App;
---
+++ SampleComponent.js
@@ -0,0 +1,51 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {Image, Text, TextInput, View} from 'react-native';
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
+      <Text>Prawito</Text>
+      <Hudoro />
+      <Text>Mila</Text>
+      <Text>Azzamy</Text>
+      <Photo />
+      <TextInput style={{borderWidth: 1}} />
+      <Profile />
+    </View>
+  );
+};
+
+const Hudoro = () => {
+  return <Text>Hudoro Mangku Negoro</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{
+        uri: 'https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+      }}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+          }}
+          style={{width: 100, height: 100, borderRadius: 50}}
+        />
+        <Text style={{color: 'blue', fontSize: 24}}>Ini Gambar</Text>
+      </View>
+    );
+  }
+}
+
+export default SampleComponent;
